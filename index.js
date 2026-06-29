e116f9b7fc276ff4b9eebd497bad6787bf397eef
yousefeid66699-afk <yousef.eid66699@gmail.com>
2026-06-29 12:55:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-/*
-Challenge: Refactor the Library System Code.
-
-You are working with a piece of code that handles 
-information about books in a library system. The 
-current code is functional but can be improved to 
-be more concise and efficient by using short-circuiting, 
-nullish coalescing, and Optional Chaining.
-
-The existing code performs the following tasks:
-- Pushes an object with title, author, year published, 
-  and availability to an array 'collection'
-- Provides default values for missing information.
-*/
-
 const collection = []
 
 function addBookToCollection(title, author, yearPublished, libraryData) {
     // Explicitly check for title
-    if (title === undefined || title === null || title === '') {
-        title = 'Unknown Title'
-    }
+    title = title || 'Unknown Title'
 
     // Explicitly check for author
-    if (author === undefined || author === null || author === '') {
-        author = 'Unknown Author'
-    }
+    author = author || 'Unknown Author'
 
     // Explicitly check if yearPublished is undefined or null
-    if (yearPublished === undefined || yearPublished === null) {
-        yearPublished = 'Not Specified'
-    }
-
-    // Explicitly check for availability at main library
-    let availability = 'Not Available' // Default value
-    if (libraryData) {
-        if (libraryData.locations) {
-            if (libraryData.locations.mainLibrary) {
-                availability = 'Available'
-            }
-        }
-    }
+    yearPublished = yearPublished ?? 'Not Specified'
+    
+    const availability = libraryData?.locations?.mainLibrary && "Available" || "Not Available"
 
     // Push the book object to 'collection'
     collection.push({
@@ -51,7 +22,7 @@ function addBookToCollection(title, author, yearPublished, libraryData) {
 }
 // Examples of adding a book
 addBookToCollection('JavaScript: The Good Parts', 'Douglas Crockford', 2008, { locations: { mainLibrary: true}})
-addBookToCollection('', 'William Shakespeare', null, null, )
+addBookToCollection('', '', null, null, )
 addBookToCollection('House of Giants', 'Gemma Small', '', null, {locations: { mainLibrary: null}})
 console.log(collection) // House of Giants is not yet published!!
 
